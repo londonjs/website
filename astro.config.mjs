e5c3a004c40ce6b55c8f9fb20e7f5a0d97051023
Shady-Prakash <shadyprakash8@gmail.com>
2025-05-15 10:52:21 +0545

    Set experimental sessional flag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,23 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 export default defineConfig({
   site: "https://london.js.org",
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [react()],
-  output: "server"
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  experimental: {
+    session: true
+  }
 });
